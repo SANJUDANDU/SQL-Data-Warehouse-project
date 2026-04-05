5c836a20373c106ebab092a3447ffb28960ba485
SANJUDANDU <sanjudandu835@gmail.com>
2026-04-05 15:20:43 +0530

    Create DDL for Silver layer tables

---
+++ scripts/Silver/DDL.Silver.sql
@@ -0,0 +1,87 @@
+--Creating DDL for Silver layer--
+--Creating table for crm.cust.info--
+
+IF OBJECT_ID('Silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE Silver.crm_cust_info;
+
+Create table Silver.crm_cust_info(
+		Cst_id INT,
+		Cst_Key NVARCHAR(50),
+		Cst_FirstName NVARCHAR(50),
+		Cst_LastName NVARCHAR(50),
+		Cst_Material_Status NVARCHAR(50),
+		Cst_Gender NVARCHAR(50),
+		Cst_Create_Date DATE,
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+--Creating table for crm.prod.info
+
+IF OBJECT_ID('Silver.crm_prod_info', 'U') IS NOT NULL
+	DROP TABLE Silver.crm_prod_info;
+
+Create table Silver.crm_prod_info(
+		Prd_id  INT,
+		cat_id NVARCHAR(50),
+		Prd_Key  NVARCHAR(50),
+		Prd_Nm  NVARCHAR(50),
+		Prd_cost  INT,
+		Prd_Line NVARCHAR(50),
+		Prd_Start_Dt DATE,
+		Prd_End_Dt DATE,
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
+	);
+
+--Creating table for crm.sales.info
+
+IF OBJECT_ID('Silver.crm_sales_info', 'U') IS NOT NULL
+	DROP TABLE Silver.crm_sales_info;
+
+Create table Silver.crm_sales_info(
+	sales_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+--Creating table for erp.cust.az12
+
+IF OBJECT_ID('Silver.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE Silver.erp_cust_az12;
+
+CREATE TABLE Silver.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	Gen NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	);
+
+--Creating table for erp.loc.a101
+
+IF OBJECT_ID('Silver.erp_LOC_A101', 'U') IS NOT NULL
+	DROP TABLE Silver.erp_LOC_A101;
+
+CREATE TABLE Silver.erp_LOC_A101(
+	CID NVARCHAR(50),
+	Country NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+--Creating table for erp.PX_CAT_G1V2
+
+IF OBJECT_ID('Silver.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+	DROP TABLE Silver.erp_PX_CAT_G1V2;
+
+CREATE TABLE Silver.erp_PX_CAT_G1V2(
+	ID NVARCHAR(50),
+	Cat NVARCHAR(50),
+	SubCat NVARCHAR(50),
+	Maintainence NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
